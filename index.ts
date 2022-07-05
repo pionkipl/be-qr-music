6bd8d0c1ca1b3c4812be81807b5db953316c9581
Jarek <j.szczepaniak@albedomarketing.pl>
2022-07-05 23:00:45 +0200

    Feat: add cors.

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
 import * as express from "express";
+import * as cors from 'cors';
 import 'express-async-errors';
+import { historyRouter } from "./routers/history";
 import './utils/db';
 import {handleError} from './utils/errors';
 
 const app = express();
 
+app.use(cors({
+  origin: process.env.ORIGIN
+}))
 app.use(handleError);
+app.use(express.json());
+
+app.use('/api', historyRouter);
 
 app.listen(3000, 'localhost', () => {
   console.log('listen on http://localhost:3000');
